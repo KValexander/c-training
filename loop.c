e62e51ba7e8d66102e534d0ad80f2044ce80281d
KValexander <ewoase@gmail.com>
2022-03-12 09:55:59 +0300

    loop upd

---
+++ loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    // Цикл for
+    // for(int i = 0; i < 10; i++) {
+    for(int i = 1; i <= 10; i++) {
+        printf("for: %d\n", i);
+    }
+
+    return 0;
+}
