fc1275794574bb4d322251ba2b775f094efb989a
KValexander <ewoase@gmail.com>
2022-04-07 10:59:11 +0300

    quiz game upd

---
+++ quiz.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+    // Вопросы
+    char questions[][100] = {
+        "1. What your first name?: ",
+        "2. What your second name?: ",
+        "3. How old are you?: "
+    };
+
+    // Варианты ответов
+    char options[][100] = {
+        "A. Alexander", "B. Noname", "C. Firstname", "D. Secondname",
+        "A. Kurchatov", "B. Secondname", "C. Nosecondname", "D. Firstname",
+        "A. 23", "B. 12", "C. 98", "D. 21"
+    };
+
+    // Правильные ответы
+    char answer[3] = {'C', 'B', 'D'};
+    // Количество вопросов
+    int count = sizeof(questions) / sizeof(questions[0]);
+
+    char guess; // Ответ
+    // Почему если я не объявлю 0, то запись чисел будет вестись некорректно?
+    int score = 0; // Количество очков
+
+    printf("QUIZ GAME\n");
+    
+    for (int i = 0; i < count; i++) {
+        printf("%s\n", questions[i]);
+        printf("******************\n");
+
+        for (int j = (i * 4); j < (i * 4) + 4; j++)
+            printf("%s\n", options[j]);
+
+        printf("Guess: ");
+        // Почему оно записывает второй символ?
+        scanf("%c", &guess);
+        scanf("%c"); // Очистка \n из буффера
+
+        // Поднять в верхний регистр
+        guess = toupper(guess);
+
+        printf("%c - %c\n", guess, answer[i]);
+
+        if (guess == answer[i]) {
+            printf("CORRECT!\n");
+            score++;
+        } else printf("WRONG!\n");
+
+        printf("\n");
+    }
+
+    printf("FINAL SCORE: %d/%d\n", score, count);
+
+    return 0;
+
+}
